0d5dcdabb7867c816c221bfd5daa60f059113a57
Paul Phillips <paulp@improving.org>
2012-03-11 00:14:16 -0500

    Fix for SI-5385.
    Nodes which hit EOF with no whitespace afterward had
    wrong position.

--- src/compiler/scala/tools/nsc/io/VirtualFile.scala
+++ src/compiler/scala/tools/nsc/io/VirtualFile.scala
@@ -54,7 +54,7 @@ class VirtualFile(val name: String, override val path: String) extends AbstractF
     }
   }
 
-  def container: AbstractFile =  unsupported
+  def container: AbstractFile = NoAbstractFile
 
   /** Is this abstract file a directory? */
   def isDirectory: Boolean = false
