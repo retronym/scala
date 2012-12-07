ebbc5a8ac9df555f09d17eb81a8ee1aa5eda88d1
Jason Zaugg <jzaugg@gmail.com>
2012-12-07 22:32:35 +0100

    Update test that was over-minimized

--- test/files/neg/t6528.scala
+++ test/files/neg/t6528.scala
@@ -1,13 +1,9 @@
+import collection.generic.Subtractable
+
 trait CoSet[U, +A <: U]
   extends CoSetLike[U, A, ({type S[A1 <: U] = CoSet[U, A1]})#S]
 
-trait CoSetLike[U, +A <: U, +This[X] <: CoSetLike[U, A, This] with CoSet[U, A]] {
-
-  implicitly[CoSet[U, Any]]
-  // should report "implicit not found"
-  // was triggering a StackOverflow as getClassParts looped over
-  // the steam of types:
-  // CoSet#6940[U#6966,A1#22868]
-  // CoSet#6940[U#6966,A1#22876]
-  // CoSet#6940[U#6966,A1#...]
+trait CoSetLike[U, +A <: U, +This[X] <: CoSetLike[U, A, This] with CoSet[U, A]]
+  extends Subtractable[U, This[U]] {
+  implicitly[CoSet[U, U] => String]
 }
