fb4b0af2799ae3f124db516d6a5192f9b1b4efcc
Jason Zaugg <jzaugg@gmail.com>
2013-03-04 13:46:08 +0100

    Move abstract Type-Map/Collector/Traverser-s
    Replicate imports from Types in the new files.

--- src/reflect/scala/reflect/internal/types/TypeComparers.scala
+++ src/reflect/scala/reflect/internal/types/TypeComparers.scala
@@ -2,7 +2,18 @@ package scala.reflect
 package internal
 package types
 
-trait TypeComparers {
-  self: Types =>
+import scala.collection.{ mutable, immutable, generic }
+import generic.Clearable
+import scala.ref.WeakReference
+import mutable.ListBuffer
+import Flags._
+import scala.util.control.ControlThrowable
+import scala.annotation.tailrec
+import util.Statistics
+import scala.runtime.ObjectRef
+import util.ThreeValues._
+import Variance._
 
+trait TypeComparers {
+  self: SymbolTable =>
 }
