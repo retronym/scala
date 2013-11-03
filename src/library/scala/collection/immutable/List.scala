58859ce36b4374fb722962613b5d7a2bfcbbc502
Jason Zaugg <jzaugg@gmail.com>
2013-11-03 16:44:36 +0100

    Remove @inline from mapConserve
    The way it is formulated with an inner tail recursive loop,
    I don't think it can ever be inlined, and just contributes a lot of
    warnings to -Yinliner-warnings.

--- src/library/scala/collection/immutable/List.scala
+++ src/library/scala/collection/immutable/List.scala
@@ -159,7 +159,7 @@ sealed abstract class List[+A] extends AbstractSeq[A]
    *  @usecase def mapConserve(f: A => A): List[A]
    *    @inheritdoc
    */
-  @inline final def mapConserve[B >: A <: AnyRef](f: A => B): List[B] = {
+  final def mapConserve[B >: A <: AnyRef](f: A => B): List[B] = {
     // Note to developers: there exists a duplication between this function and `reflect.internal.util.Collections#map2Conserve`.
     // If any successful optimization attempts or other changes are made, please rehash them there too.
     @tailrec
