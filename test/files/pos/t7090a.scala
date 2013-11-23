retronym/scala

commit 95d5554b9a263e3eb060c181463234f3e79864ab

SI-7886 unsoundness in pattern matcher.
I tracked down what was behind the issue described here:

  // TODO: fix the illegal type bound in pos/t602 -- type inference
  // messes up before we get here:
  /*override def equals(x$1: Any): Boolean = ...
         // Span[Any] --> Any is not a legal type argument for Span!
         val o5: Option[com.mosol.sl.Span[Any]] =
  */

...which led straight to the unsoundness seen in neg/t7886.
It is dangerous to have an expected type of "Any" because
the type system will blithely ignore kind errors, since "Any"
can absorb anything. The consequence in this instance was
that inferring the case constructor for a type like

  Foo[T <: Bound]

if done with expected type Any, this would come up with Foo[Any].
I altered it to use expected type Foo[T], which lets the dummy
type parameter survive to carry the bound forward and restores
sense to the inference. The before/after output for -Xprint:patmat
on pos/t602.scala is:

15c15
<         if (x1.isInstanceOf[com.mosol.sl.Span[Any]])
---
>         if (x1.isInstanceOf[com.mosol.sl.Span[K]])
17c17
<             <synthetic> val x2: com.mosol.sl.Span[Any] = \
  (x1.asInstanceOf[com.mosol.sl.Span[Any]]: com.mosol.sl.Span[Any]);
---
>             <synthetic> val x2: com.mosol.sl.Span[K] = \
  (x1.asInstanceOf[com.mosol.sl.Span[K]]: com.mosol.sl.Span[K]);
19,20c19,20
<               val low$0: Option[Any] = x2.low;
<               val high$0: Option[Any] = x2.high;
---
>               val low$0: Option[K] = x2.low;
>               val high$0: Option[K] = x2.high;

A file in the library depended (needlessly) on the unsoundness.
It was easy to fix but reminds us this may affect existing code.

src/compiler/scala/tools/nsc/typechecker/PatternTypers.scala
@@ -322,7 +322,13 @@ trait PatternTypers {
      *
      * see test/files/../t5189*.scala
      */
-    private def convertToCaseConstructor(tree: Tree, caseClass: Symbol, pt: Type): Tree = {
+    private def convertToCaseConstructor(tree: Tree, caseClass: Symbol, ptIn: Type): Tree = {
+      // Unsoundness looms for those who infer type parameters with pt=Any. See SI-7886.
+      val pt = (
+        if (ptIn =:= AnyTpe && caseClass.typeParams.nonEmpty)
+          devWarningResult(s"Evading kind-polymorphic expected type for case constructor of $caseClass")(caseClass.tpe_*)
+        else ptIn
+      )
       val variantToSkolem     = new VariantToSkolemMap
       val caseConstructorType = tree.tpe.prefix memberType caseClass memberType caseClass.primaryConstructor
       val tree1               = TypeTree(caseConstructorType) setOriginal tree

test/files/neg/t5189.check
@@ -1,5 +1,5 @@
 t5189.scala:3: error: type mismatch;
- found   : Nothing => Any
+ found   : T => U
  required: Any => Any
   def f(x: Any): Any => Any = x match { case Foo(bar) => bar }
                                                          ^

test/files/neg/t6829.check
@@ -16,12 +16,12 @@ t6829.scala:49: error: not found: value nextState
         val (s,a,s2) = (state,actions(agent),nextState)
                                              ^
 t6829.scala:50: error: type mismatch;
- found   : s.type (with underlying type Any)
+ found   : s.type (with underlying type T1)
  required: _53.State where val _53: G
         val r = rewards(agent).r(s,a,s2)
                                  ^
 t6829.scala:51: error: type mismatch;
- found   : s.type (with underlying type Any)
+ found   : s.type (with underlying type T1)
  required: _50.State
         agent.learn(s,a,s2,r): G#Agent
                     ^

src/actors/scala/actors/remote/Proxy.scala
@@ -118,7 +118,7 @@ private[remote] case class Apply0(rfun: Function2[AbstractActor, Proxy, Unit])
  */
 private[remote] class DelegateActor(creator: Proxy, node: Node, name: Symbol, kernel: NetKernel) extends Actor {
   var channelMap = new mutable.HashMap[Symbol, OutputChannel[Any]]
-  var sessionMap = new mutable.HashMap[OutputChannel[Any], Symbol]
+  var sessionMap = new mutable.HashMap[OutputChannel[_], Symbol]
 
   def act() {
     Actor.loop {

src/compiler/scala/tools/nsc/transform/patmat/MatchTranslation.scala
@@ -500,11 +500,6 @@ trait MatchTranslation {
     // U must have N members _1,..., _N -- the _i are type checked, call their type Ti,
     // for now only used for case classes -- pretending there's an unapplyProd that's the identity (and don't call it)
     class ExtractorCallProd(val fun: Tree, val args: List[Tree]) extends ExtractorCall {
-      // TODO: fix the illegal type bound in pos/t602 -- type inference messes up before we get here:
-      /*override def equals(x$1: Any): Boolean = ...
-             val o5: Option[com.mosol.sl.Span[Any]] =  // Span[Any] --> Any is not a legal type argument for Span!
-      */
-
       private def constructorTp = fun.tpe
 
       def isTyped    = fun.isTyped

test/files/neg/t4818.check
@@ -1,6 +1,6 @@
 t4818.scala:4: error: type mismatch;
  found   : Int(5)
- required: Nothing
+ required: A
 	def f(x: Any) = x match { case Fn(f) => f(5) }
                                                   ^
 one error found

test/files/neg/t7886.check
@@ -1,0 +1,6 @@
+t7886.scala:10: error: type mismatch;
+ found   : Contra[A]
+ required: Contra[Any]
+    case Unravel(m, msg) => g(m)
+                              ^
+one error found

test/files/neg/t7886.scala
@@ -1,0 +1,22 @@
+trait Covariant[+A]
+trait Contra[-A] { def accept(p: A): Unit }
+trait Invariant[A] extends Covariant[A] with Contra[A]
+
+case class Unravel[A](m: Contra[A], msg: A)
+
+object Test extends Covariant[Any] {
+  def g(m: Contra[Any]): Unit = m accept 5
+  def f(x: Any): Unit = x match {
+    case Unravel(m, msg) => g(m)
+    case _               =>
+  }
+  def main(args: Array[String]) {
+    f(Unravel[String](new Contra[String] { def accept(x: String) = x.length }, ""))
+  }
+}
+// java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.String
+//   at Test$$anon$1.accept(a.scala:18)
+//   at Test$.g(a.scala:13)
+//   at Test$.f(a.scala:15)
+//   at Test$.main(a.scala:18)
+//   at Test.main(a.scala)

commit df09d3f330ac8a9524c9ca2d08e7afdf6050b24e

WIP SI-7090 don't leak free case class tparams into pattern pts
Regressed in 95d5554b9a2 (see also a follow up, 5708e9d73)

We were using a `tpe_*` of a polymorphic case class constructor
in place of the dangerous (kind-polymorphic) expected type of `Any`,
but didn't cleanup the floating type variables after
`inferConstructorInstance`.

In this commit,
  - add the case class type paramters to the list of symbols that
    must be existentially extrapolated
  - change the way the extrapolator is used so that it doesn't
    destructively mutate the type of the parameter symbols
    of the case class. The removed comment, "no need to clone,
    this is OUR method type", wasn't accurate (anymore?)

Without the second change, we would see crosstalk between the
two matches in:

	case class Some[a](x: a)

	object test {
	  (null: Any) match {
	    case Some(x) => x: Any
	  }

	  (null: Some[String]) match {
	    case Some(x) => x: String
	  }
	}

They would work individually, but not together.

Why didn't we spot this before? I believe that it was because the
existential extrapolation would have been a noop if we inferred
the MethodType with the original symbols, because their types could
not refer to any of the temporary GADT skolems that we were
extrapoloting over.

TODO test this theory!

TODO SI-7090 is probably a second bird that could be killed with this
     stope

src/compiler/scala/tools/nsc/typechecker/PatternTypers.scala
@@ -340,14 +340,17 @@ trait PatternTypers {
 
       // simplify types without losing safety,
       // so that we get rid of unnecessary type slack, and so that error messages don't unnecessarily refer to skolems
-      val extrapolator = new ExistentialExtrapolation(freeVars)
+      val extrapolator = new ExistentialExtrapolation(freeVars ::: caseClass.typeParams)
       def extrapolate(tp: Type) = extrapolator extrapolate tp
 
       // once the containing CaseDef has been type checked (see typedCase),
       // tree1's remaining type-slack skolems will be deskolemized (to the method type parameter skolems)
       tree1 modifyType {
         case MethodType(ctorArgs, restpe) => // ctorArgs are actually in a covariant position, since this is the type of the subpatterns of the pattern represented by this Apply node
-          copyMethodType(tree1.tpe, ctorArgs map (_ modifyInfo extrapolate), extrapolate(restpe)) // no need to clone ctorArgs, this is OUR method type
+          createFromClonedSymbols(ctorArgs, tree1.tpe) {
+            (syms, tp) =>
+              copyMethodType(tree1.tpe, syms map (_ modifyInfo extrapolate), extrapolate(restpe))
+          }
         case tp => tp
       }
     }

test/files/neg/t5189.check
@@ -1,5 +1,5 @@
 t5189.scala:3: error: type mismatch;
- found   : T => U
+ found   : T => Any
  required: Any => Any
   def f(x: Any): Any => Any = x match { case Foo(bar) => bar }
                                                          ^

test/files/neg/t6829.check
@@ -16,32 +16,32 @@ t6829.scala:49: error: not found: value nextState
         val (s,a,s2) = (state,actions(agent),nextState)
                                              ^
 t6829.scala:50: error: type mismatch;
- found   : s.type (with underlying type T1)
+ found   : s.type (with underlying type Any)
  required: _53.State where val _53: G
         val r = rewards(agent).r(s,a,s2)
                                  ^
 t6829.scala:50: error: type mismatch;
- found   : a.type (with underlying type T2)
+ found   : a.type (with underlying type Any)
  required: _53.Action where val _53: G
         val r = rewards(agent).r(s,a,s2)
                                    ^
 t6829.scala:50: error: type mismatch;
- found   : s2.type (with underlying type T3)
+ found   : s2.type (with underlying type Any)
  required: _53.State where val _53: G
         val r = rewards(agent).r(s,a,s2)
                                      ^
 t6829.scala:51: error: type mismatch;
- found   : s.type (with underlying type T1)
+ found   : s.type (with underlying type Any)
  required: _50.State
         agent.learn(s,a,s2,r): G#Agent
                     ^
 t6829.scala:51: error: type mismatch;
- found   : a.type (with underlying type T2)
+ found   : a.type (with underlying type Any)
  required: _50.Action
         agent.learn(s,a,s2,r): G#Agent
                       ^
 t6829.scala:51: error: type mismatch;
- found   : s2.type (with underlying type T3)
+ found   : s2.type (with underlying type Any)
  required: _50.State
         agent.learn(s,a,s2,r): G#Agent
                         ^

test/files/pos/t7090a.scala
@@ -1,0 +1,6 @@
+case class Transition[S](x: S)
+object C
+
+class Test {
+  (??? : Any) match  { case Transition(C) => }
+}