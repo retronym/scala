3eea97dc2dcd75d92c66c305fbb310944cc6331b
Jason Zaugg <jzaugg@gmail.com>
2014-05-15 21:28:45 +0200

    Optimize array copy in Scopes

--- src/reflect/scala/reflect/internal/Scopes.scala
+++ src/reflect/scala/reflect/internal/Scopes.scala
@@ -453,7 +453,7 @@ trait Scopes extends api.Scopes { self: SymbolTable =>
     nested.elems = outer.elems
     nested.nestinglevel = outer.nestinglevel + 1
     if (outer.hashtable ne null)
-      nested.hashtable = java.util.Arrays.copyOf(outer.hashtable, outer.hashtable.length)
+      nested.hashtable = outer.hashtable.clone()
   }
 
   /** Create a new scope with given initial elements */
