98ed027886f9f954d1213834ed504f21bfb41d72
Jason Zaugg <jzaugg@gmail.com>
2015-01-29 14:45:26 +1000

    Enable GenBCode by default

--- src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
+++ src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
@@ -349,7 +349,7 @@ trait ScalaSettings extends AbsScalaSettings
    */
   val Ybackend = ChoiceSetting ("-Ybackend", "choice of bytecode emitter", "Choice of bytecode emitter.",
                                 List("GenASM", "GenBCode"),
-                                "GenASM")
+                                "GenBCode")
   // Feature extensions
   val XmacroSettings          = MultiStringSetting("-Xmacro-settings", "option", "Custom settings for macros.")
 
