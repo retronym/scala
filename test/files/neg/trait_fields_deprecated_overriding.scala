b9c64fe5385b4067783f97c72cfcfff35aed0174
Adriaan Moors <adriaan.moors@typesafe.com>
2015-11-19 11:25:09 -0800

    deprecated overriding

--- test/files/neg/trait_fields_deprecated_overriding.scala
+++ test/files/neg/trait_fields_deprecated_overriding.scala
@@ -4,6 +4,8 @@ trait DeprecatedOverriding {
   @deprecatedOverriding val x = 1
 }
 
-class C extends DeprecatedOverriding {
+class COverride extends DeprecatedOverriding {
   override val x = 2
 }
+
+class CSynthImpl extends DeprecatedOverriding
