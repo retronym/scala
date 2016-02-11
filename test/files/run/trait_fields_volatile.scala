279ae9f701eb0e9e8d0ba6371de4ee62b9fde80c
Jason Zaugg <jzaugg@gmail.com>
2016-02-11 14:42:42 +1000

    Finish writing test/files/run/trait_fields_volatile.scala

--- test/files/run/trait_fields_volatile.scala
+++ test/files/run/trait_fields_volatile.scala
@@ -1,7 +1,13 @@
-// TODO: bytecode test
-
 // bytecode should reflect volatile annotation
 trait VolatileAbort {
   @volatile private var abortflag = false
 }
 class DefaultSignalling extends VolatileAbort
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val field = classOf[DefaultSignalling].getDeclaredFields.find(_.getName.contains("abortflag")).get
+    assert(java.lang.reflect.Modifier.isVolatile(field.getModifiers), field)
+  }
+
+}
