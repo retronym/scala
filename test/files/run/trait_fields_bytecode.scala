b59cdfd7f662211249643ab9378199a01a2b359b
Jason Zaugg <jzaugg@gmail.com>
2016-02-11 14:59:48 +1000

    Add assertions to run/trait_fields_bytecode.scala

--- test/files/run/trait_fields_bytecode.scala
+++ test/files/run/trait_fields_bytecode.scala
@@ -1,5 +1,3 @@
-// TODO: bytecode test
-
 trait TFinal { final val bla: Int = 123 }
 
 // bla should be final in C
@@ -8,4 +6,18 @@ class CFinal extends TFinal
 
 trait TConst { final val C = "S" }
 // there should be a C method in `T$class`!
-class CConst extends TConst { println(C) }
+class CConst extends TConst {  }
+
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val f1 = classOf[CFinal].getDeclaredMethod("bla")
+    import java.lang.reflect.Modifier._
+    assert(isFinal(f1.getModifiers), f1)
+
+    classOf[CConst].getMethod("C")
+
+    import language.reflectiveCalls
+    assert(new CConst().asInstanceOf[{def C: String}].C == "S")
+  }
+}
