39ef34e7258761b9a40351a8cc96e1357d2b7596
Jason Zaugg <jzaugg@gmail.com>
2016-05-30 16:23:07 +1000

    Partest runner in JUnit

--- test/junit/scala/tools/nsc/backend/jvm/DirectCompileTest.scala
+++ test/junit/scala/tools/nsc/backend/jvm/DirectCompileTest.scala
@@ -113,4 +113,28 @@ class DirectCompileTest extends BytecodeTesting {
     compiler.compileToBytes("", a)
     compiler.compileToBytes(b)
   }
+
+  @Test
+  def traitBackendTest(): Unit = {
+    val compiler = newCompiler(extraArgs = "-Xprint:all")
+    val code = """|trait T extends java.lang.Iterable[String] {
+                  |  def foo = {
+                  |    super[Iterable].spliterator
+                  |    super.spliterator
+                  |  }
+                  |  def iterator(): java.util.Iterator[String] = java.util.Collections.emptyList().iterator()
+                  |}
+                  |class C extends T
+                  |object Test {
+                  |  def main(args: Array[String]): Unit = {
+                  |    val t: T = new C
+                  |    t.spliterator
+                  |    t.foo
+                  |  }
+                  |}
+                  |""".stripMargin
+    compiler.compileClasses(code)
+  }
+
+
 }
