8b00e0e6cac0fed495977aec147b0fe3b046bb04
Pavel Petlinsky <p.petlinsky@rambler-co.ru>
2016-07-08 15:37:21 +0300

    SI-9750 scala.util.Properties.isJavaAtLeast works with JDK9
    The utility method compares javaSpecVersion, which has the
    form "1.8" previously and "9" going forward.
    
    The method accepts "1.n" for n < 9. More correctly, the string
    argument should be a single number.
    
    Supports JEP-223.
    
    (cherry picked from commit 72076e59257da72f962d4101d87ff5507da28e4f)

--- test/pending/junit/scala/util/t7265.scala
+++ test/pending/junit/scala/util/t7265.scala
@@ -7,15 +7,16 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-import scala.util.PropertiesTrait
+import scala.tools.testing.AssertUtil._
 
 /** The java version property uses the spec version
- *  and must work for all "major.minor" and fail otherwise.
+ *  and must work for legacy "major.minor" and plain version_number,
+ *  and fail otherwise.
  */
 @RunWith(classOf[JUnit4])
 class SpecVersionTest {
-  val sut = new PropertiesTrait {
-    override def javaSpecVersion = "1.7"
+  class TestProperties(versionAt: String) extends PropertiesTrait {
+    override def javaSpecVersion = versionAt
 
     override protected def pickJarBasedOn: Class[_] = ???
     override protected def propCategory: String = "test"
@@ -23,24 +24,56 @@ class SpecVersionTest {
     // override because of vals like releaseVersion
     override lazy val scalaProps = new java.util.Properties
   }
+  val sut7 = new TestProperties("1.7")
+  val sut9 = new TestProperties("9")
+
+  @Test
+  def comparesJDK9Correctly(): Unit = {
+    assert(sut9 isJavaAtLeast "1")
+    assert(sut9 isJavaAtLeast "1.5")
+    assert(sut9 isJavaAtLeast "5")
+    assert(sut9 isJavaAtLeast "1.8")
+    assert(sut9 isJavaAtLeast "8")
+    assert(sut9 isJavaAtLeast "9")
+  }
 
   @Test
   def comparesCorrectly(): Unit = {
-    assert(sut isJavaAtLeast "1.5")
-    assert(sut isJavaAtLeast "1.6")
-    assert(sut isJavaAtLeast "1.7")
-    assert(!(sut isJavaAtLeast "1.8"))
+    assert(sut7 isJavaAtLeast "1")
+    assert(sut7 isJavaAtLeast "1.5")
+    assert(sut7 isJavaAtLeast "5")
+    assert(sut7 isJavaAtLeast "1.6")
+    assert(sut7 isJavaAtLeast "1.7")
+    assertFalse(sut7 isJavaAtLeast "1.8")
+    assertFalse(sut7 isJavaAtLeast "9")
+    assertFalse(sut7 isJavaAtLeast "10")
   }
+
+  @Test def variousBadVersionStrings(): Unit = {
+    assertThrows[NumberFormatException] { sut7 isJavaAtLeast "1.9" }
+    assertThrows[NumberFormatException] { sut9 isJavaAtLeast "1.9" }
+    assertThrows[NumberFormatException] { sut7 isJavaAtLeast "9.1" }
+    assertThrows[NumberFormatException] { sut9 isJavaAtLeast "9.1" }
+  }
+
   @Test(expected = classOf[NumberFormatException])
   def badVersion(): Unit = {
-    sut isJavaAtLeast "1.a"
+    sut7 isJavaAtLeast "1.a"
   }
   @Test(expected = classOf[NumberFormatException])
-  def missingVersion(): Unit = {
-    sut isJavaAtLeast "1"
+  def noVersion(): Unit = {
+    sut7 isJavaAtLeast ""
+  }
+  @Test(expected = classOf[NumberFormatException])
+  def dotOnly(): Unit = {
+    sut7 isJavaAtLeast "."
+  }
+  @Test(expected = classOf[NumberFormatException])
+  def leadingDot(): Unit = {
+    sut7 isJavaAtLeast ".5"
   }
   @Test(expected = classOf[NumberFormatException])
   def notASpec(): Unit = {
-    sut isJavaAtLeast "1.7.1"
+    sut7 isJavaAtLeast "1.7.1"
   }
 }
