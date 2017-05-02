6de2c15cff3fe42e3f5c1958fc91e9e96b1df317
Jason Zaugg <jzaugg@gmail.com>
2017-05-03 09:17:57 +1000

    Optimize CommandLineParser to avoid exponential cost

--- src/compiler/scala/tools/cmd/CommandLineParser.scala
+++ src/compiler/scala/tools/cmd/CommandLineParser.scala
@@ -35,11 +35,19 @@ object CommandLineParser {
   }
   private object DoubleQuoted extends QuotedExtractor('"')
   private object SingleQuoted extends QuotedExtractor('\'')
-  private val Word = """(\S+)(.*)""".r
+  object Word {
+    private val regex = """(\S+)""".r
+    def unapply(s: CharSequence): Option[(String, CharSequence)] = {
+      regex.findPrefixOf(s) match {
+        case Some(prefix) => Some(prefix, s.subSequence(prefix.length, s.length()))
+        case None => None
+      }
+    }
+  }
 
   // parse `in` for an argument, return it and the remainder of the input (or an error message)
   // (argument may be in single/double quotes, taking escaping into account, quotes are stripped)
-  private def argument(in: String): Either[String, (String, String)] = in match {
+  private def argument(in: CharSequence): Either[String, (String, CharSequence)] = in match {
     case DoubleQuoted(arg, rest) => Right((arg, rest))
     case SingleQuoted(arg, rest) => Right((arg, rest))
     case Word(arg, rest)         => Right((arg, rest))
@@ -47,17 +55,37 @@ object CommandLineParser {
   }
 
   // parse a list of whitespace-separated arguments (ignoring whitespace in quoted arguments)
-  @tailrec private def commandLine(in: String, accum: List[String] = Nil): Either[String, (List[String], String)] = {
-    val trimmed = in.trim
-    if (trimmed.isEmpty) Right((accum.reverse, ""))
+  @tailrec private def commandLine(in: CharSequence, accum: List[String] = Nil): Either[String, (List[String], String)] = {
+    val trimmed = trim(in)
+    if (trimmed.length() == 0) Right((accum.reverse, ""))
     else argument(trimmed) match {
       case Right((arg, next)) =>
-        (next span Character.isWhitespace) match {
-          case("", rest) if rest.nonEmpty => Left("Arguments should be separated by whitespace.") // TODO: can this happen?
-          case(ws, rest)                  => commandLine(rest, arg :: accum)
+        span(next, Character.isWhitespace) match {
+          case(ws, rest) if ws.length() == 0 && rest.length() != 0 =>
+            Left("Arguments should be separated by whitespace.") // TODO: can this happen?
+          case(ws, rest)                  => commandLine(rest, arg.toString :: accum)
         }
       case Left(msg) => Left(msg)
     }
+  }
+
+  private def trim(cs: CharSequence): CharSequence = {
+    def isSpace(c: Char) = c < '\u0020'
+    var end = cs.length
+    var start = 0
+    while ((start < end) && isSpace(cs.charAt(start)))
+      start += 1
+    while ((start < end) && isSpace(cs.charAt(end - 1)))
+      end -= 1
+    if ((start > 0) || (end < cs.length)) cs.subSequence(start, end)
+    else cs
+  }
+  private def span(cs: CharSequence, f: Char => Boolean): (CharSequence, CharSequence) = {
+    val i = prefixLength(cs, f)
+    (cs.subSequence(0, i), cs.subSequence(i, cs.length()))
+  }
+  private def prefixLength(cs: CharSequence, f: Char => Boolean): Int = {
+    (0 until cs.length()).prefixLength(i => f(cs.charAt(i)))
   }
 
   class ParseException(msg: String) extends RuntimeException(msg)
