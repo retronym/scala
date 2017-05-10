d79a3e06c1565ff2cebd3b090554f886e618f0c0
Jason Zaugg <jzaugg@gmail.com>
2017-05-10 11:05:32 +1000

    Avoid indirection in hot path in as seen from

--- src/reflect/scala/reflect/internal/tpe/TypeMaps.scala
+++ src/reflect/scala/reflect/internal/tpe/TypeMaps.scala
@@ -391,7 +391,7 @@ private[internal] trait TypeMaps {
       case tp @ ThisType(_)                                            => thisTypeAsSeen(tp)
       case tp @ SingleType(_, sym)                                     => if (sym.isPackageClass) tp else singleTypeAsSeen(tp)
       case tp @ TypeRef(_, sym, _) if isTypeParamOfEnclosingClass(sym) => classParameterAsSeen(tp)
-      case _                                                           => mapOver(tp)
+      case _                                                           => tp.mapOver(this)
     }
 
     private var _capturedSkolems: List[Symbol] = Nil
