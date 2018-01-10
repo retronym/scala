2a40af2614ced878843191346c196f282fe9eef6
Dale Wijnand <dale.wijnand@gmail.com>
2018-01-09 16:23:13 -0800

    upgrade build from sbt 0.13 -> sbt 1

--- project/VersionUtil.scala
+++ project/VersionUtil.scala
@@ -102,7 +102,6 @@ object VersionUtil {
     * suffix is used for releases. All other suffix values are treated as RC / milestone builds. The special suffix
     * value "SPLIT" is used to split the real suffix off from `baseVersion` instead and then apply the usual logic. */
   private lazy val versionPropertiesImpl: Def.Initialize[Versions] = Def.setting {
-    val log = sLog.value
     val (date, sha) = (gitProperties.value.date, gitProperties.value.sha)
 
     val (base, suffix) = {
