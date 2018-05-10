fc1e0b47d3a40ac9e9d3caae9bbbc1cdf1f6e26a
Jason Zaugg <jzaugg@gmail.com>
2018-05-10 09:18:14 +0800

    fixup one element cache

--- src/compiler/scala/tools/nsc/transform/SpecializeTypes.scala
+++ src/compiler/scala/tools/nsc/transform/SpecializeTypes.scala
@@ -1405,8 +1405,9 @@ abstract class SpecializeTypes extends InfoTransform with TypingTransformers {
                               to: List[Symbol],
                               targetClass: Symbol,
                               addressFields: Boolean) extends TreeSymSubstituter(from, to) {
+    self =>
     override val symSubst = new SubstSymMap {
-      init(from, to)
+      init(self.from, self.to)
       override def matches(sym1: Symbol, sym2: Symbol) =
         if (sym2.isTypeSkolem) sym2.deSkolemize eq sym1
         else sym1 eq sym2
