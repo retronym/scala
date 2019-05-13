526edba832aea1d48ba038b54476dd5013ad0af0
Harrison Houghton <hora.rhino@gmail.com>
2019-05-12 20:27:30 -0400

    Clarify what Null is a subtype of.
    To wit, any non-value-class class type, and non-class value types so
    declared, but certainly not value-class types.
    
    Okay, okay, that's not what I've made it say. Hopefully this is clearer.
    
    Fixes scala/bug#11479.

--- src/library-aux/scala/Null.scala
+++ src/library-aux/scala/Null.scala
@@ -13,9 +13,13 @@
 package scala
 
 /** `Null` is - together with [[scala.Nothing]] - at the bottom of the Scala type hierarchy.
- *
- *  `Null` is a subtype of all reference types; its only instance is the `null` reference.
- *  Since `Null` is not a subtype of value types, `null` is not a member of any such type.  For instance,
- *  it is not possible to assign `null` to a variable of type [[scala.Int]].
- */
+  *
+  * `Null` is the type of the `null` literal. It is a subtype of every type
+  * except those of value classes (that is, classes which are subtypes of [[AnyVal]]).
+  * This includes primitive types such as [[Int]] and [[Boolean]], as well
+  * as types of user-defined value classes.
+  *
+  * Since `Null` is not a subtype of value types, `null` is not a member of any such type.
+  * For instance, it is not possible to assign `null` to a variable of type [[scala.Int]].
+  */
 sealed trait Null
