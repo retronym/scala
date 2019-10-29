b2d9dfe47b5aec00acb1b8f4ddad06a8e24faa91
Jason Zaugg <jzaugg@gmail.com>
2019-10-29 15:38:50 +1000

    Demonstrate that a post-namer, pre-typer phase can force typeckecking
    ```
    $ cat sandbox/test.scala && qscalac -Ystop-after:earlypickler sandbox/test.scala
    package p1 {
      package p2 {
        class C {
           def foo = 42
           def bar: Int = {xxx; 42}
           class D {
              def bax: String = {yyy; 32}
           }
        }
      }
    }
    class C extends AnyRef
      def <init>(): p1.p2.C
      def foo: Int
      def bar: Int
      class D extends AnyRef
        def <init>(): C.this.D
        def bax: String
    ```

--- src/compiler/scala/tools/nsc/typechecker/Analyzer.scala
+++ src/compiler/scala/tools/nsc/typechecker/Analyzer.scala
@@ -80,13 +80,59 @@ trait Analyzer extends AnyRef
     }
   }
 
+  object earlyPickler extends {
+    val global: Analyzer.this.global.type = Analyzer.this.global
+  } with SubComponent {
+    val phaseName = "earlypickler"
+    val runsAfter = List[String]()
+    val runsRightAfter= Some("packageobjects")
+
+    def newPhase(_prev: Phase): StdPhase = new StdPhase(_prev) {
+      override val checkable = false
+      import global._
+
+      def pickle(tree: Tree): Unit = tree match {
+        case PackageDef(_, stats) =>
+          tree.symbol.initialize; stats.foreach(pickle(_))
+        case ClassDef(_, _, _, _) | ModuleDef(_, _, _) =>
+          val sym = tree.symbol.initialize
+          def shouldPickle(sym: Symbol) = currentRun.compiles(sym)
+          if (shouldPickle(sym)) {
+            val b = new java.lang.StringBuilder
+            def skip(sym: Symbol) = sym.isPrivateThis || (sym.isPrivate && !sym.owner.isTrait)
+            def printSym(sym: Symbol, level: Int): Unit = if (!skip(sym)){
+              sym.initialize
+              val indent = " " * (level * 2)
+              def p(s: String): Unit = {
+                b.append(indent)
+                b.append(s)
+                b.append("\n")
+              }
+              p(sym.defString)
+              if (sym.isClass) {
+                sym.info.decls.foreach(sym => printSym(sym, level + 1))
+              } else if (sym.isClass) {
+                sym.moduleClass.info.decls.foreach(sym => printSym(sym, level + 1))
+              }
+            }
+            printSym(sym, 0)
+            println(b.toString)
+          }
+        case _ =>
+      }
+      def apply(unit: CompilationUnit): Unit = {
+        pickle(unit.body)
+      }
+    }
+  }
+
   object typerFactory extends {
     val global: Analyzer.this.global.type = Analyzer.this.global
   } with SubComponent {
     import global.statistics
     val phaseName = "typer"
     val runsAfter = List[String]()
-    val runsRightAfter = Some("packageobjects")
+    val runsRightAfter = Some("earlypickler")
     def newPhase(prev: Phase): StdPhase = new TyperPhase(prev)
     final class TyperPhase(prev: Phase) extends StdPhase(prev) {
       override def keepsTypeParams = false
