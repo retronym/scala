a07e809ffaffac4fcbcbb5f133e4e1a9e3b580ad
Jason Zaugg <jzaugg@gmail.com>
2020-03-19 07:32:16 +1000

    Be explicit about lambdaLift following the async phase.

--- src/compiler/scala/tools/nsc/Global.scala
+++ src/compiler/scala/tools/nsc/Global.scala
@@ -583,7 +583,7 @@ class Global(var currentSettings: Settings, reporter0: Reporter)
   // phaseName = "lambdalift"
   object lambdaLift extends {
     val global: Global.this.type = Global.this
-    val runsAfter = List("posterasure")
+    val runsAfter = List("async")
     val runsRightAfter = None
   } with LambdaLift
 
